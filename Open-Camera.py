52519e5f57e30faf813f6f5929563c32433f30f6
Muhamad-Ameen <110090924+Muhamad-Ameen@users.noreply.github.com>
2022-07-27 04:35:49 -0700

    Add files via upload

---
+++ Open-Camera.py
@@ -0,0 +1,15 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+
+def camera():
+    while True:
+        success, img = cap.read()
+        cv2.cvtColor(img, cv2.COLOR_RGBA2BGR)
+        cv2.imshow('Muhamad Ameen', img)
+        if cv2.waitKey(1) & 0xff == ord('M'):
+            break
+
+
+camera()
